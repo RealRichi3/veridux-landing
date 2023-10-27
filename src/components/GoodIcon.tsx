457a7961bbf78a42bab7c2d5890cc69f507ff556
Osaretin Frank <osaretin.frank10@gmail.com>
2023-10-27 01:00:41 +0100

    deploy: fixes

--- src/components/GoodIcon.tsx
+++ src/components/GoodIcon.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export const GoodIcon = () => {
   return (
     <div>
--- src/components/TrackerCompanyIcon.tsx
+++ src/components/TrackerCompanyIcon.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export const TrackerCompanyIcon = () => {
   return (
     <span className="trackang">
