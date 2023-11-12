ec61361ffe13d2ca7294fd6e0c4a9ee435674024
RealRichi3 <molunorichie@gmail.com>
2023-11-12 22:19:17 +0100

    fix: dom errors

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -6,114 +6,114 @@ import type { CollapseProps } from "antd"
 import { Collapse } from "antd"
 
 export const FAQs = () => {
-  const text = `
+    const text = `
   coming soon
 `
 
-  const items: CollapseProps["items"] = [
-    {
-      key: "1",
-      label: (
-        <div>
-          <b>Is there a free trial available?</b>
-        </div>
-      ),
-      children: (
-        <p>
-          Yes, you can try us for free for 30 days. If you want, we’ll provide
-          you with a free, personalized 30-minute onboarding call to get you up
-          and running as soon as possible.
-        </p>
-      ),
-    },
-    {
-      key: "2",
-      label: <b>How do I change my Email Address?</b>,
-      children: <p>{text}</p>,
-    },
-    {
-      key: "3",
-      label: <b>How does billing work?</b>,
-      children: <p>{text}</p>,
-    },
-  ]
-  return (
-    <div className="faqs">
-      <Header />
-      <Hero />
+    const items: CollapseProps["items"] = [
+        {
+            key: "1",
+            label: (
+                <div>
+                    <b>Is there a free trial available?</b>
+                </div>
+            ),
+            children: (
+                <p>
+                    Yes, you can try us for free for 30 days. If you want, we’ll provide
+                    you with a free, personalized 30-minute onboarding call to get you up
+                    and running as soon as possible.
+                </p>
+            ),
+        },
+        {
+            key: "2",
+            label: <b>How do I change my Email Address?</b>,
+            children: <p>{text}</p>,
+        },
+        {
+            key: "3",
+            label: <b>How does billing work?</b>,
+            children: <p>{text}</p>,
+        },
+    ]
+    return (
+        <div className="faqs">
+            <Header />
+            <Hero />
 
-      <div className="faqs_inner">
-        <h2>Frequently asked questions</h2>
-        <p className="tagline">
-          Everything you need to know about the product and billing.
-        </p>
-        <Collapse
-          defaultActiveKey={["1"]}
-          ghost
-          items={items}
-          expandIcon={({ isActive }) =>
-            isActive ? <CollapseIcon /> : <ExpandIcon />
-          }
-          style={{ margin: "3rem 0" }}
-        />
-      </div>
-      <div
-        style={{ background: "#fff", margin: "2rem", paddingBottom: "2rem" }}
-      >
-        <div className="avatar-group">
-          <div>
-            <img src="/Avatar group.png" />
-          </div>
-          <p style={{ marginTop: "26px" }}>
-            <strong>Still have questions?</strong>
-          </p>
-          <p className="tagline">
-            Everything you need to know about the product and billing.
-          </p>
-          <Link to={"/"} className="btn-primary">
-            Get Started
-          </Link>
+            <div className="faqs_inner">
+                <h2>Frequently asked questions</h2>
+                <p className="tagline">
+                    Everything you need to know about the product and billing.
+                </p>
+                <Collapse
+                    defaultActiveKey={["1"]}
+                    ghost
+                    items={items}
+                    expandIcon={({ isActive }) =>
+                        isActive ? <CollapseIcon /> : <ExpandIcon />
+                    }
+                    style={{ margin: "3rem 0" }}
+                />
+            </div>
+            <div
+                style={{ background: "#fff", margin: "2rem", paddingBottom: "2rem" }}
+            >
+                <div className="avatar-group">
+                    <div>
+                        <img src="/Avatar group.png" />
+                    </div>
+                    <p style={{ marginTop: "26px" }}>
+                        <strong>Still have questions?</strong>
+                    </p>
+                    <p className="tagline">
+                        Everything you need to know about the product and billing.
+                    </p>
+                    <Link to={"/"} className="btn-primary">
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  )
+    )
 }
 
 export const CollapseIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-    >
-      <path
-        d="M7 11H15M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z"
-        stroke="#006FCF"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  )
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+        >
+            <path
+                d="M7 11H15M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z"
+                stroke="#006FCF"
+                stroke-width="2"
+                strokeLinecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
 }
 
 export const ExpandIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-  >
-    <path
-      d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-      stroke="#006FCF"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+    >
+        <path
+            d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+            stroke="#006FCF"
+            stroke-width="2"
+            strokeLinecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,145 +8,147 @@ import { Link } from "react-router-dom"
 import { Footer } from "../components/Footer"
 
 export const Home = () => {
-  return (
-    <div className="landingPage">
-      <Header />
-      <Hero />
-      <div className="why-choose-us">
-        <h2>Why choose our</h2>
-        <h2>platform?</h2>
-        <Row gutter={[32, 32]} style={{ margin: "35px" }}>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>Vendors:</b> Effortless Product Management
-              </span>
-            </div>
-          </Col>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>For Admins:</b> Manage User Access
-              </span>
-            </div>
-          </Col>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>Vendors:</b> Reach a wider Audience
-              </span>
-            </div>
-          </Col>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>For Admins:</b> Real-time Analytics
-              </span>
-            </div>
-          </Col>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>Vendors:</b> Secure Payment Handling
-              </span>
-            </div>
-          </Col>
-          <Col xs={24} md={12}>
-            <div className="vendor-admin-item">
-              <GoodIcon />
-              <span>
-                <b>For Admins:</b> Real-time Analytics
-              </span>
-            </div>
-          </Col>
-        </Row>
-      </div>
-      <div className="what-they-say">
-        <h2>What They say About</h2>
-        <h2>Veridux</h2>
-        <div className="what-they-say_inner">
-          <Row justify={"space-between"} align={"middle"}>
-            <Col xs={24} md={12}>
-              <img src={"/girl.png"} alt="Girl" />
-            </Col>
-            <Col xs={24} md={10}>
-              <QouteIcon />
-              <div style={{ marginTop: "30px" }}>
-                <p>"We love momaStore! Our</p>
-                <p>designers were using it for their</p>
-                <p>projects, so clients already knew</p>
-                <p>what Landingfolio was and how</p>
-                <p>to use it."</p>
-              </div>
-              <div className="reviewer">
-                <small>
-                  <b>Jenny Wilson</b>
-                </small>
-                <small style={{ color: "#52525B" }}>Co-Founder of</small>
-                <TrackerCompanyIcon />
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
+    return (
+        <div className="landingPage">
+            <Header />
+            <div className="container">
+                <Hero />
+                <div className="why-choose-us">
+                    <h2>Why choose our</h2>
+                    <h2>platform?</h2>
+                    <Row gutter={[32, 32]} style={{ margin: "35px" }}>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>Vendors:</b> Effortless Product Management
+                                </span>
+                            </div>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>For Users:</b> Access huge catalogue of products
+                                </span>
+                            </div>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>Vendors:</b> Reach a wider Audience
+                                </span>
+                            </div>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>For Users:</b> Get discounts of purchases
+                                </span>
+                            </div>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>Vendors:</b> Secure Payment Handling
+                                </span>
+                            </div>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <div className="vendor-admin-item">
+                                <GoodIcon />
+                                <span>
+                                    <b>For Users:</b> Earn bonuses on referrals
+                                </span>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="what-they-say">
+                    <h2>What They say About</h2>
+                    <h2>Veridux</h2>
+                    <div className="what-they-say_inner">
+                        <Row justify={"space-between"} align={"middle"}>
+                            <Col xs={24} md={12}>
+                                <img src={"/girl.png"} alt="Girl" />
+                            </Col>
+                            <Col xs={24} md={10}>
+                                <QouteIcon />
+                                <div style={{ marginTop: "30px" }}>
+                                    <p>"We love momaStore! Our</p>
+                                    <p>designers were using it for their</p>
+                                    <p>projects, so clients already knew</p>
+                                    <p>what Landingfolio was and how</p>
+                                    <p>to use it."</p>
+                                </div>
+                                <div className="reviewer">
+                                    <small>
+                                        <b>Jenny Wilson</b>
+                                    </small>
+                                    <small style={{ color: "#52525B" }}>Co-Founder of</small>
+                                    <TrackerCompanyIcon />
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
 
-      <div className="what-are-you-waiting-for">
-        <Row align={"middle"}>
-          <Col xs={24} md={12}>
-            <h2>What are you waiting for</h2>
-            <h2>Register now</h2>
-            <div style={{ margin: "13px 0" }}>
-              <div>
-                <p>Clarity gives you the blocks & components you need to</p>
-                <p>
-                  create a truly professional website, landing page or admin
-                </p>
-                <p>panel for your SaaS and gives the blocks.</p>
-              </div>
+                <div className="what-are-you-waiting-for">
+                    <Row align={"middle"}>
+                        <Col xs={24} md={12}>
+                            <h2>What are you waiting for</h2>
+                            <h2>Register now</h2>
+                            <div style={{ margin: "13px 0" }}>
+                                <div>
+                                    <p>Clarity gives you the blocks & components you need to</p>
+                                    <p>
+                                        create a truly professional website, landing page or admin
+                                    </p>
+                                    <p>panel for your SaaS and gives the blocks.</p>
+                                </div>
+                            </div>
+                            <div style={{ marginTop: "2.5rem" }}>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        marginBottom: "1rem",
+                                    }}
+                                >
+                                    <GoodIcon />
+                                    <span style={{ marginLeft: "15px" }}>
+                                        <b>Start Selling Today</b>
+                                    </span>
+                                </div>
+                                <div style={{ display: "flex", alignItems: "center" }}>
+                                    <GoodIcon />
+                                    <span style={{ marginLeft: "15px" }}>
+                                        <b>Bacome a Vendor</b>
+                                    </span>
+                                </div>
+                            </div>
+                            <Link
+                                to="/"
+                                className="btn-primary"
+                                style={{
+                                    borderRadius: "47px",
+                                    fontSize: "16px",
+                                    marginTop: "50px",
+                                }}
+                            >
+                                Register now
+                            </Link>
+                        </Col>
+                        <Col xs={24} md={12} className="phone2">
+                            <img src="/phone2.svg" alt="Veridux Mobile App" />
+                        </Col>
+                    </Row>
+                </div>
             </div>
-            <div style={{ marginTop: "2.5rem" }}>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  marginBottom: "1rem",
-                }}
-              >
-                <GoodIcon />
-                <span style={{ marginLeft: "15px" }}>
-                  <b>Start Selling Today</b>
-                </span>
-              </div>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <GoodIcon />
-                <span style={{ marginLeft: "15px" }}>
-                  <b>Bacome a Vendor</b>
-                </span>
-              </div>
-            </div>
-            <Link
-              to="/"
-              className="btn-primary"
-              style={{
-                borderRadius: "47px",
-                fontSize: "16px",
-                marginTop: "50px",
-              }}
-            >
-              Register now
-            </Link>
-          </Col>
-          <Col xs={24} md={12} className="phone2">
-            <img src="/phone2.svg" alt="Veridux Mobile App" />
-          </Col>
-        </Row>
-      </div>
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
