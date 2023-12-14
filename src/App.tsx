ac69d67c0141b78aefb9b3e0c8c033688b7faec8
RealRichi3 <molunorichie@gmail.com>
2023-12-14 17:50:07 +0100

    fix: redirect opening many tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { FAQs } from "./pages/FAQs"
-import PrivacyPolicy  from "./pages/PrivacyPolicy"
-import Terms from "./pages/Terms"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { FAQs } from "./pages/FAQs";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Terms from "./pages/Terms";
+
+const AppStore = () => {
+  window.location =
+    "https://play.google.com/store/apps/details?id=com.momastores.app.momastores" as string &
+      Location;
+
+  return <>{}</>;
+};
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/faqs" element={<FAQs />} />
-                    <Route path="/privacy" element={<PrivacyPolicy />} />
-                    <Route path="/terms" element={<Terms />} />
-                </Routes>
-            </Router>
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faqs" element={<FAQs />} />
+          <Route path="/android" element={<AppStore />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
